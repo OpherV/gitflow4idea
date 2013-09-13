2eea81745ef45a6c8ba410db9c56081909b1682c
OpherV <opherv@gmail.com>
2013-09-13 16:37:21 +0300

    Refactored plugin initialization according to http://devnet.jetbrains.com/thread/449233?tstart=0

--- src/gitflow/GitflowComponent.java
+++ src/gitflow/GitflowComponent.java
@@ -3,22 +3,27 @@ package gitflow;
 import com.intellij.dvcs.DvcsUtil;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
+import com.intellij.openapi.vcs.VcsListener;
+import com.intellij.util.messages.MessageBus;
 import gitflow.ui.GitflowWidget;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Opher Vishnia / opherv.com / opherv@gmail.com
  */
-public class GitflowComponent implements ProjectComponent {
+public class GitflowComponent implements ProjectComponent, VcsListener {
     Project myProject;
     GitflowWidget myGitflowWidget;
+    MessageBus messageBus;
 
     public GitflowComponent(Project project) {
         myProject = project;
     }
 
     public void initComponent() {
-
+        messageBus = myProject.getMessageBus();
+        messageBus.connect().subscribe(ProjectLevelVcsManager.VCS_CONFIGURATION_CHANGED, this);
     }
 
     public void disposeComponent() {
@@ -31,12 +36,25 @@ public class GitflowComponent implements ProjectComponent {
     }
 
     public void projectOpened() {
-        myGitflowWidget  = new GitflowWidget(myProject);
-        DvcsUtil.installStatusBarWidget(myProject, myGitflowWidget );
+
     }
 
     public void projectClosed() {
-        DvcsUtil.removeStatusBarWidget(myProject, myGitflowWidget);
-        myGitflowWidget = null;
+
+    }
+
+    @Override
+    public void directoryMappingChanged() {
+        //git repo present
+        if (ProjectLevelVcsManager.getInstance(myProject).getAllVcsRoots().length>0){
+            myGitflowWidget  = new GitflowWidget(myProject);
+            DvcsUtil.installStatusBarWidget(myProject, myGitflowWidget );
+        }
+        else{
+            if (myGitflowWidget!=null){
+                DvcsUtil.removeStatusBarWidget(myProject, myGitflowWidget);
+            }
+            myGitflowWidget = null;
+        }
     }
 }
