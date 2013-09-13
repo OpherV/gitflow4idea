2868e060b5cd4b6a697f9c72db45ae6f12264351
OpherV <opherv@gmail.com>
2013-09-13 17:27:54 +0300

    Make sure to only show the widget when a git VCS is present

--- src/gitflow/GitflowComponent.java
+++ src/gitflow/GitflowComponent.java
@@ -6,9 +6,11 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsListener;
+import com.intellij.openapi.vcs.VcsRoot;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.util.messages.MessageBus;
+import git4idea.GitVcs;
 import gitflow.ui.GitflowWidget;
 import org.jetbrains.annotations.NotNull;
 
@@ -48,8 +50,11 @@ public class GitflowComponent implements ProjectComponent, VcsListener {
 
     @Override
     public void directoryMappingChanged() {
+        VcsRoot[] vcsRoots=ProjectLevelVcsManager.getInstance(myProject).getAllVcsRoots();
+
         //git repo present
-        if (ProjectLevelVcsManager.getInstance(myProject).getAllVcsRoots().length>0){
+        if (vcsRoots.length>0 && vcsRoots[0].getVcs() instanceof GitVcs){
+
             myGitflowWidget  = new GitflowWidget(myProject);
             StatusBar statusBar = WindowManager.getInstance().getStatusBar(myProject);
             if (statusBar != null) {
