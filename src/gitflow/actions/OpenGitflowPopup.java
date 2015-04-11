614641e83d7dea62082cffec59e05e2262c4cfba
Alexander von Bremen-Kühne <avbk@neusta.de>
2015-04-11 16:00:19 +0200

    Created a new action that can be assigned to keyboard shortcut

---
+++ src/gitflow/actions/OpenGitflowPopup.java
@@ -0,0 +1,15 @@
+package gitflow.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class OpenGitflowPopup extends AnAction {
+
+    OpenGitflowPopup() {
+        super("Gitflow Operations Popup...");
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+    }
+}
