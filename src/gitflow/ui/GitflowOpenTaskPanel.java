ed6a848990e721c7692fcb000285b98196d6089e
Opher Vishnia <opherv@gmail.com>
2016-08-16 21:08:12 +0300

    UI working for OpenTaskPanel

--- src/gitflow/ui/GitflowOpenTaskPanel.java
+++ src/gitflow/ui/GitflowOpenTaskPanel.java
@@ -1,24 +1,53 @@
 package gitflow.ui;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.VcsTaskHandler;
 import com.intellij.tasks.Task;
+import com.intellij.tasks.TaskManager;
+import com.intellij.tasks.impl.TaskManagerImpl;
 import com.intellij.tasks.ui.TaskDialogPanel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-/**
- * Created by opherv on 8/16/16.
- */
-public class GitflowOpenTaskPanel extends TaskDialogPanel {
+public class GitflowOpenTaskPanel extends TaskDialogPanel implements ItemListener {
+    private JRadioButton noActionRadioButton;
     private JRadioButton startFeatureRadioButton;
     private JTextField featureName;
-    private JRadioButton noActionRadioButton;
     private JRadioButton startHotfixRadioButton;
-    private JPanel myPanel;
     private JTextField hotfixName;
+    private JPanel myPanel;
+
+    private TaskManagerImpl myTaskManager;
+    private VcsTaskHandler myVcsTaskHandler;
+
 
     public GitflowOpenTaskPanel(Project project, Task task){
+        myTaskManager = (TaskManagerImpl) TaskManager.getManager(project);
+        VcsTaskHandler[] vcsTaskHAndlers = VcsTaskHandler.getAllHandlers(project);
+        if (vcsTaskHAndlers.length > 0){
+            //todo handle case of multiple vcs handlers
+            myVcsTaskHandler = vcsTaskHAndlers[0];
+        }
+
+        String branchName = myVcsTaskHandler != null
+                                            ? myVcsTaskHandler.cleanUpBranchName(myTaskManager.constructDefaultBranchName(task))
+                                            : myTaskManager.suggestBranchName(task);
+
+        featureName.setText(branchName);
+        featureName.setEditable(false);
+        featureName.setEnabled(false);
+
+        hotfixName.setText(branchName);
+        hotfixName.setEditable(false);
+        hotfixName.setEnabled(false);
+
+        //add listeners
+        noActionRadioButton.addItemListener(this);
+        startFeatureRadioButton.addItemListener(this);
+        startHotfixRadioButton.addItemListener(this);
 
     }
 
@@ -30,6 +59,35 @@ public class GitflowOpenTaskPanel extends TaskDialogPanel {
 
     @Override
     public void commit() {
+
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        Object source = e.getItemSelectable();
+
+        //disable\enable textfields based on radio selection
+        if (e.getStateChange() == ItemEvent.SELECTED && source == noActionRadioButton) {
+            featureName.setEditable(false);
+            featureName.setEnabled(false);
+
+            hotfixName.setEditable(false);
+            hotfixName.setEnabled(false);
+        }
+        else if (e.getStateChange() == ItemEvent.SELECTED && source == startFeatureRadioButton) {
+            featureName.setEditable(true);
+            featureName.setEnabled(true);
+
+            hotfixName.setEditable(false);
+            hotfixName.setEnabled(false);
+        }
+        else if (e.getStateChange() == ItemEvent.SELECTED && source == startHotfixRadioButton) {
+            featureName.setEditable(false);
+            featureName.setEnabled(false);
+
+            hotfixName.setEditable(true);
+            hotfixName.setEnabled(true);
+        }
 
     }
 }
