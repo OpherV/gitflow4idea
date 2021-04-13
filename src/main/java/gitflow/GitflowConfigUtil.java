9b37a87e9f401374a889ece1f6fc17b046aad61d
OpherV <opherv@gmail.com>
2021-04-13 14:29:28 +0300

    Fix "Error using shortcuts" #322

--- src/main/java/gitflow/GitflowConfigUtil.java
+++ src/main/java/gitflow/GitflowConfigUtil.java
@@ -31,7 +31,7 @@ public class GitflowConfigUtil {
     public static final String PREFIX_SUPPORT = "gitflow.prefix.support";
     public static final String PREFIX_VERSIONTAG = "gitflow.prefix.versiontag";
 
-    private static  Map<Project, Map<GitRepository, GitflowConfigUtil>> gitflowConfigUtilMap = new HashMap<Project, Map<GitRepository, GitflowConfigUtil>>();
+    private static  Map<Project, Map<String, GitflowConfigUtil>> gitflowConfigUtilMap = new HashMap<Project, Map<String, GitflowConfigUtil>>();
 
     Project project;
     GitRepository repo;
@@ -47,14 +47,14 @@ public class GitflowConfigUtil {
     public static GitflowConfigUtil getInstance(Project project_, GitRepository repo_)
     {
         GitflowConfigUtil instance;
-        if (gitflowConfigUtilMap.containsKey(project_) && gitflowConfigUtilMap.get(project_).containsKey(repo_)) {
-            instance = gitflowConfigUtilMap.get(project_).get(repo_);
+        if (gitflowConfigUtilMap.containsKey(project_) && gitflowConfigUtilMap.get(project_).containsKey(repo_.getPresentableUrl())) {
+            instance = gitflowConfigUtilMap.get(project_).get(repo_.getPresentableUrl());
         } else {
-            Map<GitRepository, GitflowConfigUtil> innerMap = new HashMap<GitRepository, GitflowConfigUtil>();
+            Map<String, GitflowConfigUtil> innerMap = new HashMap<String, GitflowConfigUtil>();
             instance = new GitflowConfigUtil(project_, repo_);
 
             gitflowConfigUtilMap.put(project_, innerMap);
-            innerMap.put(repo_, instance);
+            innerMap.put(repo_.getPresentableUrl(), instance);
 
             //cleanup
             Disposer.register(repo_, () -> innerMap.remove(repo_));
